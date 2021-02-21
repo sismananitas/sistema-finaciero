3e0349f38bf581dcc4f2fa55d1a7262e0fed8cfe
Jesus Adame <desarrollo@lasmananitas.com.mx>
2021-02-20 23:22:14 -0600

    Spatie laravel permissions istalled

---
+++ app/Providers/InertiaServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\ServiceProvider;
+use Inertia\Inertia;
+
+class InertiaServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // User permissions
+        Inertia::share('can', function () {
+            if (!empty(Auth::user())) {
+                return [
+                    'index_users'        => Auth::user()->can('index_users'),
+                    'index_roles'        => Auth::user()->can('index_roles'),
+                    'index_accounts'     => Auth::user()->can('index_clients'),
+                    'control_panel'      => Auth::user()->can('control_panel'),
+                ];
+            }
+        });
+
+        // Navbar routes
+        Inertia::share("navbar_routes", function () {
+            return [
+                [ "name" => "home", "url" => '/home', "text"=> "Inicio" ],
+                [ "name" => "accounts.index", "url" => '/cuentas', "text" => "Cuentas" ],
+            ];
+        });
+    }
+}
